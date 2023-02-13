1e319b8c16e9e2a79826da1b618d81e1ab7cbe65
gkavin57 <gkavin77@gmail.com>
2023-02-13 12:07:37 +0530

    update b

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,8 +3,8 @@ export const headerData = {
   title: "Hello! I am",
   desciption:
     "Hello! I am KAVIN, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
-  image: "https://i.ibb.co/ZmdNH6x/abu-saidpg",
-  imagebw: "https://i.ibb.co/Cw2Xp90/abu-sai",
+  image: "https://i.ibb.co/C7ZrJ6F/IMG-20210408-WA0011-01.jpg",
+
   resumePdf:
     "https://docs.google.com/document/d/1MkkoRX98FS47CaEyeodyPzi4OkW8SH7Gv55f4MQkHV8/edit?uring",
 };
